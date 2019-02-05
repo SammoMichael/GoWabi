8024a6102f27fba9a45313c53d0bc86b2e26e61a
Samuel Michael <samadammichael@yahoo.com>
2019-02-05 18:32:31 -0500

    conditionally render icons and infowindow

---
+++ GoWabi/frontend/components/newmap/Marker.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Marker extends Component {
+  render() {
+    return (
+      <div>
+        "hello world"
+      </div>
+    );
+  }
+}
+
+export default Marker;
+
--- GoWabi/frontend/components/newmap/newmap.jsx
+++ GoWabi/frontend/components/newmap/newmap.jsx
@@ -1,38 +1,57 @@
 import GoogleMapReact from 'google-map-react';
-import { GoogleApiWrapper } from 'google-maps-react';
+import { GoogleApiWrapper, InfoWindow } from "google-maps-react";
 import React from 'react';
+import Marker from './Marker';
 
 export default class NewMap extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            showingInfoWindow: false,  //Hides or the shows the infoWindow
+            activeMarker: {},          //Shows the active marker upon click
+            selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+        };
     }
 
     componentDidMount() {
         this.props.fetchLocations();
+        const iconUrls = {
+            spa: "",
+            barber: "",
+            removal: "",
+            beauty: "",
+        }; 
     }
     
     render() {
-        debugger;
-        // const markers = this.props.locations.map((loc) => (
-        //     <Marker className="markers"
-        //         key={loc.id} >
-        //         <DeckIndexItem
-        //             key={deck.id}
-        //             deck={deck} /> </Link>
-        // ));
+        if(!this.props.locations) {
+          return  (<div>loading</div>)
+        } else {
+        const markers = this.props.locations.map((loc) => (
+            <Marker 
+                // position={{ lat: loc.lat, lng: loc.lng }}
+                lat={loc.lat}
+                lng={loc.lng}
+                text={loc.category}
+                key={loc.id} />
+            
+        ));
         return (
-            <div style={{ height: "80vh", width: "60%" }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{
-                        key: "AIzaSyBdKGFAJbGj2l9yy3V68oPp7cMR1pxS1s0",
-                        language: "english"
-                    }}
-                    defaultCenter={{ lat: 40.6944, lng: -73.9213 }}
-                    defaultZoom={11}
-                    className="new-map"
-                />
-            </div>
-        );
+          <div style={{ height: "80vh", width: "60%" }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: "AIzaSyBdKGFAJbGj2l9yy3V68oPp7cMR1pxS1s0",
+                language: "english"
+              }}
+              defaultCenter={{ lat: 40.6944, lng: -73.9213 }}
+              defaultZoom={11}
+              className="new-map"
+            >
+                    {markers}
+              
+            </GoogleMapReact>
+          </div>
+            );
+        }
     }
 }
